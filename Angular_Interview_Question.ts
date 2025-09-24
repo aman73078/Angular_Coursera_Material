2bda0951ff2f4da902d2357ebb7591031f68aeda
anjali-tripathi1 <123200498+aman73078@users.noreply.github.com>
2025-09-25 01:08:36 +0530

    Added 10 interview questions

---
+++ Angular_Interview_Question.ts
@@ -0,0 +1,19 @@
+// 1.  What is Angular, and what are its primary uses?
+
+// 2.  Explain the key differences between Angular and its predecessor, AngularJS.
+
+// 3.   What are the main building blocks of an Angular application? Can you name at least three?
+
+// 4.   Describe the architecture of an Angular application. How do components fit into this architecture?
+
+// 5.   What is the purpose of Angular modules (ngModules), and how do they help in organizing an application?
+
+// 6.   How does Angular promote a standardized structure across projects, and what are the benefits of this approach?
+
+// 7.   What is the significance of TypeScript in Angular development?
+
+// 8.   Can you explain what declarative control flow is and how it enhances UI creation in Angular?
+
+// 9.   What are some built-in performance enhancements provided by Angular?
+
+// 10.  How does Angular facilitate testing, and what framework is included for this purpose?
